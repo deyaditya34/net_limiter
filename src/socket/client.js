71c197836b35171b9d47ec86474896865e2d3545
Aditya Dey <deyaditya34@gmail.com>
2026-09-24 14:45:22 +0530

    refactored the code and updated the socket closing event handlers

--- src/socket/client.js
+++ src/socket/client.js
@@ -1,124 +1,33 @@
 import net from "net";
+import readline from "readline";
 import { encodeMessage, decodeMessage } from "./protocol.js";
+import { parser } from "./cliParser.js";
 
 const SOCKET_PATH = "test.sock";
 
-const requests = [
-	{
-		id: 1,
-		command: "usage",
-		options: {
-			days: 30
-		}
-	},
-	{
-		id: 2,
-		command: "usage",
-		options: {
-			days: 2
-		}
-	},
-	{
-		id: 3,
-		command: "usage",
-		options: {
-			from: "2026-09-01",
-			to: "2026-09-18"
-		}
-	},
-	{
-		id: 4,
-		command: "usage",
-		options: {
-			days: 350
-		}
-	},
-	{
-		id: 5,
-		command: "interface",
-		options: {
-			days: 30
-		}
-	},
-	{
-		id: 6,
-		command: "interface",
-		options: {
-			from: "2026-09-01",
-			to: "2026-09-19"
-		}
-	},
-	{
-		id: 7,
-		command: "speed"
-	},
-	{
-		id: 8,
-		command: "limit",
-		subCommand: "get"
-	},
-	{
-		id: 9,
-		command: "limit",
-		subCommand: "set",
-		options: {
-			days: 30,
-			amount: 1
-		},
-	},
-	{
-		id: 10,
-		command: "limit",
-		subCommand: "set",
-		options: {
-			from: "2026-09-20",
-			to: "2026-10-20",
-			amount: 2,
-		},
-	},
-	{
-		id: 11,
-		command: "limit",
-		subCommand: "kuch bhi"
-	},
-	{
-		id: 12,
-		command: "limit",
-		subCommand: "get"
-	},
-	{
-		id: 13,
-		command: "session"
-	},
-	{
-		id: 14,
-		command: "status"
-	},
-	{
-		id: 15,
-		command: "notification",
-		subCommand: "disable",
-		options: {
-			threshold: 2
-		}
-	},
-	{
-		id: 16,
-		command: "notification",
-		subCommand: "enable"
-	},
-	{
-		id: 17,
-		command: "help"
-	}
-]
-
 const client = net.createConnection(SOCKET_PATH, () => {
-	console.log("connected to server");
+	console.log("connecting to the net limiter server");
+});
 
-	for (const request of requests) {
-		client.write(encodeMessage(request));
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+	prompt: "netlimiter > "
+});
+
+client.on("connect", () => {
+	console.log("Connected to net limiter server");
+	rl.prompt();
+});
+
+rl.on("line", (input) => {
+	if (input.trim === "") {
+		rl.prompt();
+		return;
 	}
+
+	const request = parser(input);
+	client.write(encodeMessage(request));
 });
 
 let buffer = "";
@@ -133,14 +42,29 @@ client.on("data", (data) => {
 		if (message.length === 0) continue;
 
 		const response = decodeMessage(message);
-		console.log("response -", response);
+		console.log("netlimiter > response -", response);
+		rl.prompt();
 	}
 })
 
+client.on("close", () => {
+	console.log("close event: Disconnected from server");
+	rl.close();
+	process.exit(0);
+});
+
 client.on("end", () => {
-	console.log("connection closed");
+	console.log("end event: connection closed");
 });
 
 client.on("error", (err) => {
-	console.error("Socket error -", err.message);
+	console.error("socket error -", err.message);
+});
+
+process.on("SIGINT", () => {
+	client.end();
+});
+
+process.on("SIGTERM", () => {
+	client.end();
 });
