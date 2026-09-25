2491d250b231d0decab841c82cbba790645be17f
Aditya Dey <deyaditya34@gmail.com>
2026-09-25 23:36:36 +0530

    message parsing logic between server socket and client socket uses the same protocol

--- src/socket/client.js
+++ src/socket/client.js
@@ -1,6 +1,6 @@
 import net from "net";
-import { encodeMessage, decodeMessage } from "./protocol.js";
-import { parser } from "./cliParser.js";
+import { encodeMessage, decodeMessage, parseMessages } from "./protocol.js";
+import { cliParser } from "../cli/cliParser.js";
 
 const SOCKET_PATH = "test.sock";
 
@@ -10,39 +10,37 @@ const client = net.createConnection(SOCKET_PATH, () => {
 let input = process.argv.slice(2).join(" ");
 
 if (input.trim() === "") {
-	input = "--help";
+	input = "help";
 }
 
-const request = parser(input);
+const request = cliParser(input);
 client.write(encodeMessage(request));
 
 let buffer = "";
 client.on("data", (data) => {
-	buffer += data.toString();
+	const result = parseMessages(buffer, data);
+	buffer = result.buffer;
 
-	let newLineIndex;
-	while ((newLineIndex = buffer.indexOf("\n")) !== -1) {
-		const message = buffer.slice(0, newLineIndex);
-		buffer = buffer.slice(newLineIndex + 1);
-
-		if (message.length === 0) continue;
+	for (const message of result.messages) {
 
 		const response = decodeMessage(message);
 		console.log("netlimiter > response -", response);
+
+		client.end();
 	}
 })
 
 client.on("close", () => {
-//	console.log("close event: Disconnected from server");
+	//	console.log("close event: Disconnected from server");
 	process.exit(0);
 });
 
 client.on("end", () => {
-//	console.log("end event: connection closed");
+	//	console.log("end event: connection closed");
 });
 
 client.on("error", (err) => {
-console.error("socket error -", err.message);
+	console.error("socket error -", err.message);
 });
 
 process.on("SIGINT", () => {
